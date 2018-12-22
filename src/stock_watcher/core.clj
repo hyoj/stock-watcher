669fc63e09ff75aed569b103a6d3b698c880a572
hyoj <chohyojoy@gmail.com>
2018-12-22 17:03:04 +0900

    Add function which brings stock price

--- src/stock_watcher/core.clj
+++ src/stock_watcher/core.clj
@@ -1,7 +1,15 @@
 (ns stock-watcher.core
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.string :as string]))
 
-(let [resp (client/get "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=035420")
-      body (:body resp)]
-  (println body))
+(defn zip-str [s]
+  (zip/xml-zip
+    (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
+;; get current stock price
+(defn get-stock-price [stock-code]
+  (zip-str
+    (string/trim
+      (:body (client/get (str "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=" stock-code))))))
