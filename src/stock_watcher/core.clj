06a67abefc13e8d118c64c96f9cdad80a3f35b3e
hyoj <chohyojoy@gmail.com>
2018-12-22 17:33:52 +0900

    Add function which brings current stock price

--- src/stock_watcher/core.clj
+++ src/stock_watcher/core.clj
@@ -8,8 +8,19 @@
   (zip/xml-zip
     (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
-;; get current stock price
+(def base-url "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=")
+
+;; get stock price
 (defn get-stock-price [stock-code]
   (zip-str
     (string/trim
-      (:body (client/get (str "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=" stock-code))))))
+      (:body (client/get (str base-url stock-code))))))
+
+;; get current stock price
+(defn get-current-stock-price [stock-code]
+  (-> (get-stock-price stock-code)
+      first
+      :content
+      (get 2)
+      :attrs
+      :CurJuka))
