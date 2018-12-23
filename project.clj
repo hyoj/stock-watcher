64b3aefa8157e20c08f0f3ba0e4e9292d2cb9b51
hyoj <chohyojoy@gmail.com>
2018-12-23 21:37:14 +0900

    Add Telegram bot

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.9.1"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.9.1"]
+                 [morse "0.4.0"]]
+  :jvm-opts ["-Dfile.encoding=utf-8"])
--- src/stock_watcher/core.clj
+++ src/stock_watcher/core.clj
@@ -2,19 +2,27 @@
   (:require [clj-http.client :as client]
             [clojure.xml :as xml]
             [clojure.zip :as zip]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [morse.handlers :as h]
+            [morse.api :as api]
+            [morse.polling :as p]))
 
 (defn zip-str [s]
   (zip/xml-zip
     (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
+(def token (System/getenv "TELEGRAM_TOKEN"))
 (def base-url "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=")
+(def base-en-url "http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code=")
 
 ;; get stock price
 (defn get-stock-price [stock-code]
   (zip-str
     (string/trim
-      (:body (client/get (str base-url stock-code))))))
+      (:body (client/get (str base-en-url stock-code))))))
+
+(get-stock-price "000830")                                  ;
+(get-stock-price "036460")                                  ; 한국가스공사
 
 ;; get current stock price
 (defn get-current-stock-price [stock-code]
@@ -24,3 +32,29 @@
       (get 2)
       :attrs
       :CurJuka))
+
+(get-current-stock-price "036460")
+
+(h/defhandler bot-api
+              (h/command "start" {{username :username} :from {id :id :as chat} :chat}
+                         (println "User" username "joined")
+                         (api/send-text token id {:parse_mode "html"}
+                                        (str "안녕하세요. <b><i>" username "</i></b> 님\n"
+                                             "개발중인 bot 입니다.")))
+
+              (h/command "hello" {{id :id :as chat} :chat}
+                         (println "hello was requested in " chat)
+                         (api/send-text token id {:parse_mode "Markdown"}
+                                        "*Hello*, fellows :)"))
+
+              ;(h/command "" {{id :id :as chat} :chat}
+              ;           (println "hello was requested in " chat)
+              ;           (api/send-text token id {:parse_mode "Markdown"}
+              ;                          "*Hello*, fellows :)"))
+
+              (h/message message (println "Intercepted message:" message)))
+
+(def channel (p/start token bot-api))
+
+;stop created background processes
+;(p/stop channel)
