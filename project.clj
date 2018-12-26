efd4324797d99f9673874dcb6ab2e89f0bbd6bbe
hyoj <chohyojoy@gmail.com>
2018-12-26 15:18:59 +0900

    Add subscribe feature

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.9.1"]
-                 [morse "0.4.0"]]
+                 [morse "0.4.0"]
+                 [clojurewerkz/quartzite "2.1.0"]]
   :jvm-opts ["-Dfile.encoding=utf-8"]
   :main stock-watcher.core
   :profiles {:uberjar {:aot :all}})
--- src/stock_watcher/core.clj
+++ src/stock_watcher/core.clj
@@ -6,7 +6,12 @@
             [clojure.string :as string]
             [morse.handlers :as h]
             [morse.api :as api]
-            [morse.polling :as p]))
+            [morse.polling :as p]
+            [clojurewerkz.quartzite.scheduler :as qs]
+            [clojurewerkz.quartzite.triggers :as t]
+            [clojurewerkz.quartzite.jobs :as j]
+            [clojurewerkz.quartzite.jobs :refer [defjob]]
+            [clojurewerkz.quartzite.schedule.daily-interval :refer [schedule monday-through-friday starting-daily-at time-of-day ending-daily-at with-interval-in-minutes with-interval-in-seconds]]))
 
 (defn zip-str [s]
   (zip/xml-zip
@@ -22,9 +27,6 @@
     (string/trim
       (:body (client/get (str base-en-url stock-code))))))
 
-;(get-stock-price "000830")                                  ;
-;(get-stock-price "036460")                                  ; 한국가스공사
-
 ;; get current stock price
 (defn get-current-stock-price [stock-code]
   (-> (get-stock-price stock-code)
@@ -34,10 +36,12 @@
       :attrs
       :CurJuka))
 
-;(get-current-stock-price "036460")
-
+;; check stock code is valid
 (defn check-stock-code [stock-code]
   (not (nil? (re-seq #"^\d{6}$" stock-code))))
+
+; {"stock-code1" ("telegram-chat-id1" "telegram-chat-id2")}
+(def data (atom {}))
 
 (h/defhandler bot-api
               (h/command "start" {{username :username} :from {id :id :as chat} :chat}
@@ -51,10 +55,22 @@
                          (api/send-text token id {:parse_mode "Markdown"}
                                         "*Hello*, fellows :)"))
 
+              (h/command "subscribe" {{id :id :as chat} :chat text :text}
+                         (println "subscribe was requested in " chat text)
+                         (let [stock-code (first (re-seq #"\d{6}" text))]
+                           (if (check-stock-code stock-code)
+                             (do
+                               (swap! data update stock-code conj id)
+                               (api/send-text token id
+                                              (str "주식종목코드 " stock-code " 알림이 시작되었습니다.\n"
+                                                   "매 30분 마다 현재가 알림을 받게됩니다.")))
+                             (api/send-text token id "유효하지 않은 주식종목코드 입니다."))))
+
               (h/message {{id :id :as chat} :chat text :text :as message}
                          (println "Intercepted message:" message)
                          (when (check-stock-code text)
                            (api/send-text token id (get-current-stock-price text)))))
+
 
 (declare channel)
 
@@ -74,6 +90,31 @@
 
 ;(restart-bot)
 
-(defn -main []
+(defjob send-current-stock-price-to-bot
+        [ctx]
+        (doseq [[stock-code chat-ids] @data]
+          (doseq [chat-id chat-ids]
+            (api/send-text token chat-id {:parse_mode "html"}
+                           (str "주식종목코드 <b>" stock-code "</b>\n"
+                                "현재가: <i>"(get-current-stock-price stock-code) "</i>" )))
+          (prn stock-code chat-ids))
+        (println "`send-current-stock-price-to-bot` working..")
+        )
+
+(defn -main [& m]
   (start-bot)
-  (while true (Thread/sleep 10000)))
+  (let [s   (-> (qs/initialize) qs/start)
+        job (j/build
+              (j/of-type send-current-stock-price-to-bot)
+              (j/with-identity (j/key "jobs.noop.1")))
+        trigger (t/build
+                  (t/with-identity (t/key "triggers.1"))
+                  (t/start-now)
+                  (t/with-schedule (schedule
+                                     (with-interval-in-seconds 5)
+                                     (monday-through-friday)
+                                     (starting-daily-at (time-of-day 9 00 00))
+                                     (ending-daily-at (time-of-day 16 00 00)))))]
+    (qs/schedule s job trigger))
+  )
+
