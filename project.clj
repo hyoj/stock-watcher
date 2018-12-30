c466bc278d5fa583f5f592feba152b518900a32e
hyoj <chohyojoy@gmail.com>
2018-12-30 23:18:32 +0900

    Add dependencies for test

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/test.check "0.9.0"]
                  [clj-http "3.9.1"]
                  [morse "0.4.0"]
                  [org.clojure/data.json "0.2.6"]
--- test/stock_watcher/core_test.clj
+++ test/stock_watcher/core_test.clj
@@ -1,6 +1,10 @@
 (ns stock-watcher.core-test
   (:require [clojure.test :refer :all]
-            [stock-watcher.core :refer :all]))
+            [stock-watcher.core :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
